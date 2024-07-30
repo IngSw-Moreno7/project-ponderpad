06e6e7077431a2e9ec63c3c8bdfe6bf0eeab9458
IngSw-Moreno7 <paolamorepi@gmail.com>
2024-07-29 21:08:42 -0500

    Integration with user

--- frontend/ponderpad-app/src/components/Cards/ProfileInfo.jsx
+++ frontend/ponderpad-app/src/components/Cards/ProfileInfo.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { getInitials } from '../../utils/helper';
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
   return (
-    <div className="flex items-center gap-3">
-      <div className="w-12 h-12 flex items=center justify-center rounded-full text-slate-950 font-medium bg-slate-100">{getInitials("Paola Moreno")}
+    userInfo && (
+      <div className="flex items-center gap-3">
+        <div className="w-12 h-12 flex items=center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+          {getInitials(userInfo?.name)}
+        </div>
 
+        <div>
+          <p className="text-sm font-medium">{userInfo?.name}</p>
+          <button className="text-sm text-slate-700 underline" onClick={onLogout}>
+            Cerrar sesión
+          </button>
+        </div>
       </div>
-
-      <div>
-        <p className="text-sm font-medium">Moreno</p>
-        <button className="text-sm text-slate-700 underline" onClick={onLogout}>
-          Cerrar sesión
-        </button>
-      </div>
-    </div>
+    )
   );
 };
 
--- frontend/ponderpad-app/src/pages/Home/Home.jsx
+++ frontend/ponderpad-app/src/pages/Home/Home.jsx
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import NoteCard from '../../components/Cards/NoteCard';
 import { MdAdd } from 'react-icons/md';
 import AddEditNotes from './AddEditNotes';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosinstance';
+import moment from "moment";
 
 const Home = () => {
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
+  const [allNotes, setAllNotes] = useState([]);
+  const [userInfo, setUserInfo] = useState(null);
+
+  const navigate = useNavigate();
+
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("get-all-notes");
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.error("error");
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo();
+    getAllNotes();
+    return () => { };
+  }, []);
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar userInfo={userInfo}></Navbar>
 
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
-          <NoteCard
-            title="AHHH"
-            date="3rd april"
-            content="aksjfl kjafkljasf lkjaf"
-            tags="#metting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          ></NoteCard>
+          {allNotes.map((item, index) => (
+            <NoteCard
+              key={item._id}
+              title={item.title}
+              date={moment(item.createdOn).format('Do MMM YYYY')}
+              content={item.content}
+              tags={item.tags}
+              isPinned={item.isPinned}
+              onEdit={() => { }}
+              onDelete={() => { }}
+              onPinNote={() => { }}
+            ></NoteCard>
+          ))}
         </div>
       </div>
 
