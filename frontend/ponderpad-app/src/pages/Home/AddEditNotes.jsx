7a1583c09bc7e19371ae767e9653c4a6b40799eb
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-07-29 21:03:25 -0500

    UPDATE CODE

--- frontend/ponderpad-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/ponderpad-app/src/pages/Home/AddEditNotes.jsx
@@ -2,31 +2,36 @@ import React from 'react';
 
 const AddEditNotes = () => {
   return (
-    <div>
+    <div className="container mx-auto p-4">
       <div className="flex flex-col gap-2">
-        <label className="input-label">TITULO</label>
+        <label className="input-label">TÍTULO</label>
         <input
           type="text"
           className="text-2xl text-slate-950 outline-none"
-          placeholder="xxxc"
         />
       </div>
+      
       <div className="flex flex-col gap-2 mt-4">
-        <label className="input-label"> CONTENIDO</label>
+        <label className="input-label">CONTENIDO</label>
         <textarea
-            type="text"
-            className='text-5m text-slate-950 outline-none bg-slate-50 p-2 rounded'
-            rows={10}
+          className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+          rows={10}
         />
-        </div>
-
-        <div className='mt-3'>
-            <label className="input-label">TAGS</label>
-        </div>
-
-        <button className='btn-primary font-medium mt-5 p-3' onClick={() => {}}>AGREGAR</button>
+      </div>
+      
+      <div className="flex flex-col gap-2 mt-4">
+        <label className="input-label">TAGS</label>
+        <input
+          type="text"
+          className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+        />
+      </div>
+      
+      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+        AGREGAR
+      </button>
+    </div>
   );
 };
 
 export default AddEditNotes;
-
