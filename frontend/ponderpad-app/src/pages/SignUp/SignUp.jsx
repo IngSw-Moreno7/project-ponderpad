9c523034b63de50f7d4edf7f119bc5e34624c295
IngSw-Moreno7 <paolamorepi@gmail.com>
2024-07-29 19:27:10 -0500

    test(wip): wip

--- frontend/ponderpad-app/src/pages/SignUp/SignUp.jsx
+++ frontend/ponderpad-app/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PasswordInput from '../../components/Input/PasswordInput';
+import Navbar from '../../components/Navbar/Navbar';
+import { Link } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!name) {
+      setError("Entre un usuario valido porfavor...");
+      return;
+    }
+    else if (!validateEmail(email)) {
+      setError("Entre un correo valido porfavor...");
+      return;
+    }
+    else if (!password) {
+      setError("Ingrese una contrase;a porfavor");
+      return;
+    }
+    setError("")
+  }
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-12">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">SignUp</h4>
+
+            <input
+              type="text"
+              placeholder="Nombre"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <input
+              type="text"
+              placeholder="Correo Electrónico"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)}></PasswordInput>
+
+            {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              Registrate
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Ya se encuentra registrado? {''}
+              <Link to="/login" className="font-medium text-primary underline">
+                login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default SignUp;
